f4ed7b14079dfd74e5de3dfa8855df9dd2caa875
EduardoReis7 <eduardodovigireis@gmail.com>
2020-08-04 18:08:54 -0300

    Commit inicial

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+// Corpo (Request Body) : Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
